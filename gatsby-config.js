d7f855f76ad1c0ddb3ce8ecb91d44bad917ffce0
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-03-29 21:08:33 +0300

    add  gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
+    " gatsby-transformer-remark",
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
