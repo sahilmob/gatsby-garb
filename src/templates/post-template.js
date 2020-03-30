4a01d3f35917a6125c38a6b26b4837b2ed2c2307
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-03-30 21:34:24 +0300

    add time to read

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -8,12 +8,16 @@ export default function PostTemplate({
     markdownRemark: {
       frontmatter: { title },
       html,
+      timeToRead,
     },
   },
 }) {
   return (
     <Layout>
       <h1>{title}</h1>
+      <h4>
+        {timeToRead} {timeToRead > 1 ? "minutes" : "minute"}
+      </h4>
       <div dangerouslySetInnerHTML={{ __html: html }} />
     </Layout>
   );
@@ -23,6 +27,7 @@ export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
+      timeToRead
       frontmatter {
         title
       }
