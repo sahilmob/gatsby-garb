4e4c1700d46335c6634bacae9ccf46cbba64f3ca
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-03-30 21:55:08 +0300

    add logo

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,8 @@ import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 
+import gatsbyLogo from "../images/gatsby-icon.png";
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
@@ -16,17 +18,29 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+      <span style={{ display: "flex", alignItems: "center" }}>
+        <img
+          src={gatsbyLogo}
+          alt="gatsby garb logo"
           style={{
-            color: `white`,
-            textDecoration: `none`,
+            width: "50px",
+            margin: "0 5px",
+            border: "3px solid orange",
+            borderRadius: "50%",
           }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+        />
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </span>
     </div>
   </header>
 );
